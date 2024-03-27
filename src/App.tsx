1529a7748fe5257119225e588f8f520c3c0800ba
Richard <richardfelipelives@gmail.com>
2024-03-27 15:24:14 -0300

    consertando App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-// import { Provider } from 'react-redux'
-// import StyleGlobal, { Container } from './styles'
-// import store from './store'
-
+import { Provider } from 'react-redux'
 import ListaDeContatos from './containers/ListaDeContatos'
+import StyleGlobal, { Container } from './styles'
+import store from './store'
 
 function App() {
   return (
-    
-      
+    <Provider store={store}>
+      <StyleGlobal>
+      <Container>
       <ListaDeContatos />
-      
+      </Container>
+    </StyleGlobal>
+    </Provider>
   )
 }
 
